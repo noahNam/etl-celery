f66583888dbc72866a388f62d6aad12c1e49586f
Harry <harry@apartalk.com>
2022-05-13 15:30:54 +0900

    feat: simple kapt spider

---
+++ modules/adapter/infrastructure/crawler/crawler/enum/kapt_enum.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+
+class KaptEnum(Enum):
+    BASE_INFO_END_POINT = (
+        "http://apis.data.go.kr/1613000/AptBasisInfoService1/getAphusBassInfo"
+    )
+    DETAIL_INFO_END_POINT = (
+        "http://apis.data.go.kr/1613000/AptBasisInfoService1/getAphusDtlInfo"
+    )
+    SERVICE_KEY = "toLAcbnBlhSZDGMeOK8gyApaeLsaa0qrLC3urCU0k%2BtyV9%2BNA9%2FwfIYqsfF9Hi2FBcxMuJBobhGNlCAyRam%2FjQ%3D%3D"
