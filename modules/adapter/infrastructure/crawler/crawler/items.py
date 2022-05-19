69713de5c56ad7ebf31d1ec20fa5039c29769aa9
Harry <harry@apartalk.com>
2022-05-19 16:38:55 +0900

    feat: kapt 수집, pipeline 전달

--- modules/adapter/infrastructure/crawler/crawler/items.py
+++ modules/adapter/infrastructure/crawler/crawler/items.py
@@ -14,7 +14,24 @@ class CrawlerItem(scrapy.Item):
 
 
 class KaptBasisInfoItem(BaseModel):
-    url: str = Field()
     kapt_code: str = Field()
-    kapt_name: str = Field()
-    kapt_addr: str = Field()
+    name: str = Field()
+    kapt_tarea: str | None = Field()
+    kapt_marea: str | None = Field()
+    kapt_mparea_60: str | None = Field()
+    kapt_mparea_85: str | None = Field()
+    kapt_mparea_135: str | None = Field()
+    kapt_mparea_136: str | None = Field()
+    priv_area: str | None = Field()
+    bjd_code: str | None = Field()
+
+
+class KaptLocationInfoItem(BaseModel):
+    kapt_code: str = Field()
+    name: str | None = Field()
+    kaptd_wtimebus: str | None = Field()
+    subway_line: str | None = Field()
+    subway_station: str | None = Field()
+    kaptd_wtimesub: str | None = Field()
+    convenient_facility: str | None = Field()
+    education_facility: str | None = Field()
