8d4e5e7942e79f43f81727eeefeb41785351a1fa
Harry <harry@apartalk.com>
2022-05-19 16:19:17 +0900

    feat: xml_to_dict -> Pipeline Model 연결

--- modules/adapter/infrastructure/crawler/crawler/items.py
+++ modules/adapter/infrastructure/crawler/crawler/items.py
@@ -4,9 +4,17 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from pydantic import BaseModel, Field
 
 
 class CrawlerItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+
+class KaptBasisInfoItem(BaseModel):
+    url: str = Field()
+    kapt_code: str = Field()
+    kapt_name: str = Field()
+    kapt_addr: str = Field()
--- modules/adapter/infrastructure/crawler/crawler/pipelines.py
+++ modules/adapter/infrastructure/crawler/crawler/pipelines.py
@@ -24,4 +24,5 @@ class BasePipeline:
 class KaptPipeline(BasePipeline):
     def process_item(self, item, spider: Spider):
         logger.info(f"item :  !!!!!!!!!!!!!!!!")
+        print(item)
         return item
