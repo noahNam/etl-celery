6018084928e4abbddc7b349372960aab878a9f11
Noah Nam <noah@apartalk.com>
2022-05-30 17:52:27 +0900

    DL 사진관련 schema model skeleton

---
+++ core/domain/photo/interface/photo_repository.py
@@ -0,0 +1,21 @@
+from abc import ABC, abstractmethod
+
+from modules.adapter.infrastructure.sqlalchemy.persistence.model.datalake.house_photo_model import HousePhotoModel
+from modules.adapter.infrastructure.sqlalchemy.persistence.model.datalake.house_type_photo_model import \
+    HouseTypePhotoModel
+from modules.adapter.infrastructure.sqlalchemy.persistence.model.datalake.public_sale_detail_photo_model import \
+    PublicSaleDetailPhotoModel
+from modules.adapter.infrastructure.sqlalchemy.persistence.model.datalake.public_sale_photo_model import \
+    PublicSalePhotoModel
+
+
+class PhotoRepository(ABC):
+    @abstractmethod
+    def save(
+        self,
+        model: HousePhotoModel
+        | HouseTypePhotoModel
+        | PublicSalePhotoModel
+        | PublicSaleDetailPhotoModel
+    ) -> None:
+        pass
