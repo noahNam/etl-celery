3afaa90d5c01d860f1efc6458364d5882c5388b6
Harry <harry@apartalk.com>
2022-06-09 16:21:20 +0900

    feat: govt_bld_spider skeleton

--- modules/adapter/infrastructure/crawler/crawler/spiders/govt_bld_spider.py
+++ modules/adapter/infrastructure/crawler/crawler/spiders/govt_bld_spider.py
@@ -1,4 +1,4 @@
-from scrapy import Spider
+from scrapy import Spider, Request
 
 from modules.adapter.infrastructure.crawler.crawler.enum.govt_bld_enum import (
     GovtBldEnum,
@@ -23,3 +23,71 @@ class GovtBldSpider(Spider):
             GovtBldEnum.GOVT_BLD_MID_URL.value,
             GovtBldEnum.GOVT_BLD_AREA_URL.value,
         ]
+
+        for param in self.params:
+            yield Request(
+                url=urls[0]
+                + f"?kaptCode={param.kapt_code}&ServiceKey={GovtBldSpider.open_api_service_key}",
+                callback=self.parse_bld_top_info,
+                errback=self.error_callback_bld_top_info,
+                meta={
+                    "house_id": param.house_id,
+                    "kapt_code": param.kapt_code,
+                    "name": param.name,
+                    "origin_dong_address": param.origin_dong_address,
+                    "new_dong_address": param.new_dong_address,
+                    "bjd_code": param.bjd_code,
+                    "url": urls[0]
+                    + f"?kaptCode={param.kapt_code}&ServiceKey={GovtBldSpider.open_api_service_key}",
+                },
+            )
+            yield Request(
+                url=urls[1]
+                    + f"?kaptCode={param.kapt_code}&ServiceKey={GovtBldSpider.open_api_service_key}",
+                callback=self.parse_bld_mid_info,
+                errback=self.error_callback_bld_mid_info,
+                meta={
+                    "house_id": param.house_id,
+                    "kapt_code": param.kapt_code,
+                    "name": param.name,
+                    "origin_dong_address": param.origin_dong_address,
+                    "new_dong_address": param.new_dong_address,
+                    "bjd_code": param.bjd_code,
+                    "url": urls[0]
+                           + f"?kaptCode={param.kapt_code}&ServiceKey={GovtBldSpider.open_api_service_key}",
+                },
+            )
+            yield Request(
+                url=urls[2]
+                    + f"?kaptCode={param.kapt_code}&ServiceKey={GovtBldSpider.open_api_service_key}",
+                callback=self.parse_bld_area_info,
+                errback=self.error_callback_bld_area_info,
+                meta={
+                    "house_id": param.house_id,
+                    "kapt_code": param.kapt_code,
+                    "name": param.name,
+                    "origin_dong_address": param.origin_dong_address,
+                    "new_dong_address": param.new_dong_address,
+                    "bjd_code": param.bjd_code,
+                    "url": urls[0]
+                           + f"?kaptCode={param.kapt_code}&ServiceKey={GovtBldSpider.open_api_service_key}",
+                },
+            )
+
+    def parse_bld_top_info(self, response, **kwargs):
+        pass
+
+    def parse_bld_mid_info(self, response, **kwargs):
+        pass
+
+    def parse_bld_area_info(self, response, **kwargs):
+        pass
+
+    def error_callback_bld_top_info(self, failure):
+        pass
+
+    def error_callback_bld_mid_info(self, failure):
+        pass
+
+    def error_callback_bld_area_info(self, failure):
+        pass
