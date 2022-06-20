f5072da9eef820beeb21721cb69ddccfd926c580
Noah Nam <noah@apartalk.com>
2022-06-20 19:21:58 +0900

    (ETL) dong_infos, type_infos to datamart

--- modules/adapter/infrastructure/sqlalchemy/persistence/model/warehouse/dong_info_model.py
+++ modules/adapter/infrastructure/sqlalchemy/persistence/model/warehouse/dong_info_model.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, BigInteger, Integer, String, Numeric, ForeignKey, Boolean
+from sqlalchemy import Column, BigInteger, Integer, String, Numeric, ForeignKey, Boolean, SmallInteger
 
+from modules.adapter.infrastructure.sqlalchemy.entity.warehouse.v1.basic_info_entity import DongInfoEntity
 from modules.adapter.infrastructure.sqlalchemy.mapper import warehouse_base
 from modules.adapter.infrastructure.sqlalchemy.persistence.model.mixins.timestamp_mixin import (
     TimestampMixin,
@@ -22,6 +23,16 @@ class DongInfoModel(warehouse_base, TimestampMixin):
         BigInteger, ForeignKey(BasicInfoModel.house_id), nullable=False, index=True
     )
     name = Column(String(30), nullable=True)
-    hhld_cnt = Column(Numeric(5), nullable=True)
-    grnd_flr_cnt = Column(Numeric(5), nullable=True)
+    hhld_cnt = Column(SmallInteger, nullable=True)
+    grnd_flr_cnt = Column(SmallInteger, nullable=True)
     update_needed = Column(Boolean, nullable=False, default=True)
+
+    def to_dong_info_entity(self) -> DongInfoEntity:
+        return DongInfoEntity(
+            id=self.id,
+            house_id=self.house_id,
+            name=self.name,
+            hhld_cnt=self.hhld_cnt,
+            grnd_flr_cnt=self.grnd_flr_cnt,
+            update_needed=self.update_needed,
+        )
