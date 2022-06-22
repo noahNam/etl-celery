4f8155f182932ba26826bdfba765cc2cc59215ec
Harry <harry@apartalk.com>
2022-06-22 15:22:15 +0900

    feat: govt_house_deal_spider skeleton

--- modules/adapter/infrastructure/crawler/crawler/enum/govt_deal_enum.py
+++ modules/adapter/infrastructure/crawler/crawler/enum/govt_deal_enum.py
@@ -1,7 +1,9 @@
 from enum import Enum
 
 
-class GovtDealEnum(Enum):
+class GovtHouseDealEnum(Enum):
+    """특정 년도, 월 범위로 범위 조정시, MIN_YEAR_MONTH, MAX_YEAR_MONTH 값 수정 후 크롤러 실행"""
+
     MIN_YEAR_MONTH = "2006-01"
     MAX_YEAR_MONTH = "2022-06"
     APT_DEAL_END_POINT = "http://openapi.molit.go.kr/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTradeDev"
@@ -13,5 +15,5 @@ class GovtDealEnum(Enum):
     OPCTL_RENT_END_POINT = "http://openapi.molit.go.kr/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcOffiRent"
     SERVICE_KEY_1 = "toLAcbnBlhSZDGMeOK8gyApaeLsaa0qrLC3urCU0k%2BtyV9%2BNA9%2FwfIYqsfF9Hi2FBcxMuJBobhGNlCAyRam%2FjQ%3D%3D"
     SERVICE_KEY_2 = "DupoKfKzoSLgBFwamlL4goP7CWP4F8e9I0OL%2Fk4lxr%2FYZrAysoaK6vlFKGVmVvqDzQrnWiFrOvlPt7FWWGHFxg%3D%3D"
-    NUMBER_OF_ROWS = 1000
+    NUMBER_OF_ROWS = 10000
     DAILY_REQUEST_COUNT = 1000
--- modules/adapter/infrastructure/crawler/crawler/spiders/govt_deal_spider.py
+++ modules/adapter/infrastructure/crawler/crawler/spiders/govt_deal_spider.py
@@ -1,27 +1,28 @@
-from scrapy import Spider
+from scrapy import Spider, Request
 
 from modules.adapter.infrastructure.crawler.crawler.enum.govt_deal_enum import (
-    GovtDealEnum,
+    GovtHouseDealEnum,
 )
-
-
-class GovtHouseDealSpider(Spider):
-    name = "govt_house_deal_infos"
-    custom_settings = {
-        "ITEM_PIPELINES": {
-            "modules.adapter.infrastructure.crawler.crawler.pipelines.GovtHouseDealPipeline": 300
-        },
-    }
-    open_api_service_key = GovtDealEnum.SERVICE_KEY_1.value
-    request_count: int = 0
+from modules.adapter.infrastructure.crawler.crawler.items import GovtHouseDealInputInfo
+from modules.adapter.infrastructure.pypubsub.enum.call_failure_history_enum import (
+    CallFailureTopicEnum,
+)
+from modules.adapter.infrastructure.pypubsub.event_listener import event_listener_dict
+from modules.adapter.infrastructure.pypubsub.event_observer import send_message
+from modules.adapter.infrastructure.sqlalchemy.entity.datalake.v1.legal_dong_code_entity import (
+    LegalDongCodeEntity,
+)
+from modules.adapter.infrastructure.sqlalchemy.persistence.model.datalake.call_failure_history_model import (
+    CallFailureHistoryModel,
+)
 
 
 class GovtDateCounter:
     def __init__(self):
-        self._min_year: int = int(GovtDealEnum.MIN_YEAR_MONTH.value.split("-")[0])
-        self._min_month: int = int(GovtDealEnum.MIN_YEAR_MONTH.value.split("-")[1])
-        self._max_year: int = int(GovtDealEnum.MAX_YEAR_MONTH.value.split("-")[0])
-        self._max_month: int = int(GovtDealEnum.MAX_YEAR_MONTH.value.split("-")[1])
+        self._min_year: int = int(GovtHouseDealEnum.MIN_YEAR_MONTH.value.split("-")[0])
+        self._min_month: int = int(GovtHouseDealEnum.MIN_YEAR_MONTH.value.split("-")[1])
+        self._max_year: int = int(GovtHouseDealEnum.MAX_YEAR_MONTH.value.split("-")[0])
+        self._max_month: int = int(GovtHouseDealEnum.MAX_YEAR_MONTH.value.split("-")[1])
         self._current_year: int = self._min_year
         self._current_month: int = self._min_month
 
@@ -47,10 +48,163 @@ class GovtDateCounter:
         self._current_year += 1
         return self._current_year
 
-    def plus_month(self) -> int:
+    def plus_month(self) -> str:
         if 0 < self._current_month < 12:
             self._current_month += 1
         else:
             self._plus_year()
             self._current_month = 1
-        return self._current_month
+        return self.current_ym()
+
+    def reset_counter(self) -> None:
+        self._current_year: int = self._min_year
+        self._current_month: int = self._min_month
+
+    def get_max_ym(self) -> str:
+        if 0 < self._max_month < 10:
+            return str(self._max_year) + "0" + str(self._max_month)
+        else:
+            return str(self._max_year) + str(self._max_month)
+
+
+class GovtHouseDealSpider(Spider):
+    name = "govt_house_deal_infos"
+    custom_settings = {
+        "ITEM_PIPELINES": {
+            "modules.adapter.infrastructure.crawler.crawler.pipelines.GovtHouseDealPipeline": 300
+        },
+    }
+    open_api_service_key = GovtHouseDealEnum.SERVICE_KEY_1.value
+    request_count: int = 0
+    date_counter: GovtDateCounter = GovtDateCounter()
+
+    def start_requests(self):
+        """self.params: list[LegalDongCodeEntity] from GovtDealUseCase class"""
+        urls: list = [
+            GovtHouseDealEnum.APT_DEAL_END_POINT.value,
+            GovtHouseDealEnum.APT_RENT_END_POINT.value,
+            GovtHouseDealEnum.OPCTL_DEAL_END_POINT.value,
+            GovtHouseDealEnum.OPCTL_RENT_END_POINT.value,
+            GovtHouseDealEnum.APT_RIGHT_LOT_OUT_END_POINT.value,
+        ]
+
+        # 법정동 코드 앞 5자리 추출
+        input_params: list[GovtHouseDealInputInfo] | None = self.get_input_infos(
+            deal_info_list=self.params
+        )
+
+        if input_params:
+            # param: 추출된 법정동 코드 앞 5자리(277개)
+            for param in input_params:
+                # current_ym: 200601 ~ 202206 (기간 조정시 GovtHouseDealEnum 값 조정)
+                while (
+                    GovtHouseDealSpider.date_counter.current_ym()
+                    <= GovtHouseDealSpider.date_counter.get_max_ym()
+                ):
+                    yield Request(
+                        url=urls[0]
+                        + f"?ServiceKey={GovtHouseDealSpider.open_api_service_key}"
+                        f"&LAWD_CD={param.bjd_front_code}"
+                        f"&pageNo=1"
+                        f"&numOfRows={GovtHouseDealEnum.NUMBER_OF_ROWS.value}"
+                        f"&DEAL_YMD={GovtHouseDealSpider.date_counter.current_ym()}",
+                        callback=self.parse_apt_deal_info,
+                        errback=self.error_callback_apt_deal_info,
+                        meta={
+                            "url": urls[0],
+                            "bjd_front_code": param,
+                            "year_month": GovtHouseDealSpider.date_counter.current_ym(),
+                        },
+                    )
+
+                    GovtHouseDealSpider.date_counter.plus_month()
+                GovtHouseDealSpider.date_counter.reset_counter()
+
+    def parse_apt_deal_info(self):
+        pass
+
+    def parse_apt_rent_info(self):
+        pass
+
+    def parse_opctl_deal_info(self):
+        pass
+
+    def parse_opctl_rent_info(self):
+        pass
+
+    def parse_apt_right_lot_out_info(self):
+        pass
+
+    def error_callback_apt_deal_info(self):
+        pass
+
+    def error_callback_apt_rent_info(self):
+        pass
+
+    def error_callback_opctl_deal_info(self):
+        pass
+
+    def error_callback_opctl_rent_info(self):
+        pass
+
+    def error_callback_apt_right_lot_out_info(self):
+        pass
+
+    def get_input_infos(
+        self, deal_info_list: list[LegalDongCodeEntity]
+    ) -> list[GovtHouseDealInputInfo]:
+        extracted: list[str] = list()
+        for info in deal_info_list:
+            extracted.append(info.region_cd[:5])
+
+        # 중복 제거
+        duplication_removed: list[str] = list(set(extracted))
+
+        result: list[GovtHouseDealInputInfo] = list()
+
+        for elm in duplication_removed:
+            result.append(GovtHouseDealInputInfo(bjd_front_code=elm))
+
+        return result
+
+    def change_service_key(self):
+        if (
+            GovtHouseDealSpider.open_api_service_key
+            == GovtHouseDealEnum.SERVICE_KEY_2.value
+        ):
+            GovtHouseDealSpider.open_api_service_key = (
+                GovtHouseDealEnum.SERVICE_KEY_1.value
+            )
+        else:
+            GovtHouseDealSpider.open_api_service_key = (
+                GovtHouseDealEnum.SERVICE_KEY_2.value
+            )
+
+    def count_requests(self):
+        if (
+            GovtHouseDealSpider.request_count
+            >= GovtHouseDealEnum.DAILY_REQUEST_COUNT.value
+        ):
+            self.change_service_key()
+            GovtHouseDealSpider.request_count = 0
+        else:
+            GovtHouseDealSpider.request_count += 1
+
+    def save_failure_info(self):
+        pass
+
+    def __save_crawling_failure(self, fail_orm: CallFailureHistoryModel) -> None:
+        send_message(
+            topic_name=CallFailureTopicEnum.SAVE_CRAWLING_FAILURE.value,
+            fail_orm=fail_orm,
+        )
+        event_listener_dict.get(
+            f"{CallFailureTopicEnum.SAVE_CRAWLING_FAILURE.value}", None
+        )
+
+    def __is_exists_failure(self, fail_orm: CallFailureHistoryModel | None) -> bool:
+        send_message(
+            topic_name=CallFailureTopicEnum.IS_EXISTS.value,
+            fail_orm=fail_orm,
+        )
+        return event_listener_dict.get(f"{CallFailureTopicEnum.IS_EXISTS.value}")
