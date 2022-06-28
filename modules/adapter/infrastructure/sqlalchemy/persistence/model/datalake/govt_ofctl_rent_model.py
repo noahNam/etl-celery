fdb0616e7058338b91399a7517fb0e28032c4eaf
Harry <harry@apartalk.com>
2022-06-28 18:07:47 +0900

    feat: 실거래가 관련 모델 index 추가

--- modules/adapter/infrastructure/sqlalchemy/persistence/model/datalake/govt_ofctl_rent_model.py
+++ modules/adapter/infrastructure/sqlalchemy/persistence/model/datalake/govt_ofctl_rent_model.py
@@ -21,12 +21,12 @@ class GovtOfctlRentModel(datalake_base, TimestampMixin):
         primary_key=True,
         autoincrement=True,
     )
-    deal_year = Column(String(4), nullable=True)
+    deal_year = Column(String(4), nullable=True, index=True)
     ofctl_name = Column(String(40), nullable=True)
     dong = Column(String(40), nullable=True)
     deposit = Column(Integer, nullable=True)
     sigungu = Column(String(40), nullable=True)
-    deal_month = Column(String(2), nullable=True)
+    deal_month = Column(String(2), nullable=True, index=True)
     deal_day = Column(String(6), nullable=True)
     monthly_amount = Column(Integer, nullable=True)
     exclusive_area = Column(String(20), nullable=True)
