004d07f89db2ad7a4e8349b2ed11417bf5006d03
jsh <sam@apartalk.com>
2022-07-12 17:18:02 +0900

    TB-1086: 전처리기 테스트12 - key 소진시 예외처리2

--- modules/adapter/infrastructure/crawler/crawler/enum/kakao_enum.py
+++ modules/adapter/infrastructure/crawler/crawler/enum/kakao_enum.py
@@ -6,8 +6,6 @@ class KakaoApiEnum(Enum):
     KAKAO_PLACE_API_URL_NO_PARAM = "https://dapi.kakao.com/v2/local/search/keyword.json"
     KAKAO_API_KEY = "5da8fd5b2fecd850a415680330cd3524"
     KAKAO_API_KEYS = [
+        "5da8fd5b2fecd850a415680330cd3524",
         "3b8170930d797a1517e33c8ed456d570",
-        "308910f9cd565c2d9e6b45091812f5ff",
-        "16f23d61ef7bef8cbdea926f63c7f78b",
-        "0ed0526c4fa4859f9adc513eed350670",
     ]
