57a916b05b24b5c965dbe25798fd1b0bbcfb0f01
Harry <harry@apartalk.com>
2022-07-14 20:22:14 +0900

    datalake: subscription_info 크롤러 upsert 개선

--- modules/adapter/infrastructure/pypubsub/enum/subs_info_enum.py
+++ modules/adapter/infrastructure/pypubsub/enum/subs_info_enum.py
@@ -3,3 +3,6 @@ from enum import Enum
 
 class SubsInfoTopicEnum(Enum):
     BULK_SAVE_SUBSCRIPTION_INFOS = "bulk_save_subscription_infos"
+    UPDATE_TO_NEW_SCHEMA = "update_to_new_schema"
+    SAVE_ALL = "save_all"
+    FIND_SUBSCRIPTION_INFOS_BY_YEAR_MONTH = "find_subscription_infos_by_year_month"
